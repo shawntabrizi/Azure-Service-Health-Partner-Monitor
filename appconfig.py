d21f002382600be3f8b852e38630278053514cfb
Shawn Tabrizi <deturbanator@gmail.com>
2017-07-08 16:22:12 -0700

    updating instructions

--- appconfig.py
+++ appconfig.py
@@ -1,17 +1,18 @@
 # This is your Application's Configuration File.
 # Make sure not to upload this file!
 
-#Flask App Secret. Used for "session".
+# Flask App Secret. Used for "session".
 flaskSecret = "<generateKey>"
 
 # Register your V1 app at https://portal.azure.com.
-# Make sure to add 2 Reply URLs to your application:
-# <domain>/partner/login/authorized    i.e. http://localhost:5000/partner/login/authorized
-# <domain>/customer/login/authorized   i.e. http://localhost:5000/customer/login/authorized
+# Sign-On URL as <domain>/customer/login/authorized i.e. http://localhost:5000/customer/login/authorized
+# Make the Application Multi-Tenant
+# Add access to Windows Azure Service Management API
+# Create an App Key
 clientId = "<GUID>"
 clientSecret = "<SECRET>"
 
-#The various resource endpoints. You may need to update this for different cloud environments.
+# The various resource endpoints. You may need to update this for different cloud environments.
 aad_endpoint = "https://login.microsoftonline.com/"
 resource_arm = "https://management.azure.com/"
 resource_graph = "https://graph.windows.net/"
